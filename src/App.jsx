e3171857d9b96c51b44edd45848590b7dd749b0d
Henry <h85033898@gmail.com>
2026-02-16 17:15:54 +0100

    thank God

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import GetHelp from "./pages/GetHelp";
-import Events from "./pages/events"; // Import your Events component
+import Events from "./pages/events";
 import Layout from "./layouts/Layout";
 import ScrollToTop from "./components/ScrollToTop";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import MyTickets from "./pages/MyTickets";
+
 function App() {
   return (
     <>
       <ScrollToTop />
-      <Routes>
+      <Routes basename="/Ticket-Event-Project-defence-"> 
         <Route element={<Layout />}>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/events" element={<Events />} /> {/* Add this line */}
+          <Route path="/events" element={<Events />} />
           <Route path="/GetHelp" element={<GetHelp />} />
           <Route path="/SignUp" element={<SignUp />} />
           <Route path="/profile" element={<Profile />} />

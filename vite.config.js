eca0ea3fc92371dabb4be03b30f734bbcd0fc952
Henry <h85033898@gmail.com>
2026-02-17 02:45:23 +0100

    testing 1

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/Ticket-Event-Project-defence-/",
+  base: "./", 
   plugins: [react(), tailwindcss()],
 });
